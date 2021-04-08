5f47792df50e80f2f37b6d66a3ed12baadddaefa
Siajiza <>
2021-04-08 11:31:52 +0000

    Add text differentiation two possible prints

--- main.py
+++ main.py
@@ -2,5 +2,9 @@ sentence = 'The quick brown fox jumped over the lazy dog.'
 
 sentence = sentence.replace('quick', 'slow')
 
-# print(sentence.replace('quick', 'slow'))
+# First possibility to print
+print(sentence.replace('quick', 'slow'))
+
+# Second possibility to print
 print(sentence)
+
